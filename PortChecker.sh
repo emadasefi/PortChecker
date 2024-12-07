302a4bafa77171da85b42dfda93bd12b97c5a4be
Emad Asefi <emad.asefi@gmail.com>
2024-12-07 16:46:45 +0330

    Update PortChecker.sh

--- PortChecker.sh
+++ PortChecker.sh
@@ -5,19 +5,45 @@ Purple='\033[0;34m'
 Cyan='\033[0;36m'
 Yellow='\033[0;33m'
 NC='\033[0m'
-####################################################
+
+
 # Get the hostname and current date and time
 current_time=$(date '+%Y-%m-%d %H:%M:%S')
 hostname=$(hostname)
-####################################################
+localip=$(hostname -I)
+
+
+# Print the ASCII art and message
+echo -e "${Cyan}
+   ___           __  _______           __          
+  / _ \___  ____/ /_/ ___/ /  ___ ____/ /_____ ____
+ / ___/ _ \/ __/ __/ /__/ _ \/ -_) __/  '_/ -_) __/
+/_/   \___/_/  \__/\___/_//_/\__/\__/_/\_\\__/_/   
+                                                   
+"
+echo -e "
+  - Port Checker Script
+  - The Port Checker Script is a Bash utility that provides an overview of active network connections on a Linux system. 
+    It uses the netstat command to gather and format key information about each connection
+  - v1.1 / Emad Asefi / @emadasefi / emad.asefi@gmail.com
+  ${Yellow}||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+  ||| Execution Time : $current_time
+  ||| Hostname : $hostname
+  ||| Local IP Address : $localip
+  ||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+${NC}"
+
+
 # Get the netstat output and format it
 netstat_output=$(sudo netstat -tulpn | awk 'NR>2 {print $1, $4, $7, $5, $6}')
-####################################################
+
+
 # Prepare the table header with fixed column widths
 printf "|------------|-------------------|----------------------|---------------------|-----------|------------------------------|\n"
 printf "| %-10s | %-17s | %-20s | %-18s | %-10s | %-30s|\n" "Protocol" "Local Address" "PID/Program Name" "Foreign Address" "State" "Location"
 printf "|------------|-------------------|----------------------|---------------------|-----------|------------------------------|\n"
-####################################################
+
+
 # Process each line of the netstat output
 while read -r line; do
     # Extract fields using awk
@@ -33,13 +59,10 @@ while read -r line; do
     # Print the formatted output with fixed column widths
     printf "| %-10s | %-17s | %-20s | %-18s | %-10s | %-30s|\n" "$proto" "$local_addr" "$pid_program" "$foreign_addr" "$state" "$location"
 done <<< "$netstat_output"
-####################################################
+
+
 #printf "|            |                   |                      |                     |           |                              |                         |\n"
 printf "|-------------------------------------------------------------------------------------------------------------------------|\n"
 printf "\n"
-####################################################
-# Print the execution time and hostname in the last row
-echo -e "| ||||||| Execution Time : $current_time"
-echo -e "| ||||||| Hostname : $hostname"
-echo -e "${Cyan}| ||||||| Author : Emad Asefi <emad.asefi@gmail.com>"
-echo -e "${NC}"
+
+
